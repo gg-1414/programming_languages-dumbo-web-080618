bd9dd22ddd530b810c4e6af900c92924ae3473e7
Gina Lee <ginalee1114@gmail.com>
2018-08-05 19:24:00 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -41,11 +41,13 @@ def reformat_languages(languages)
         language_data[language][:style] = [style]
       end 
     elsif style == :functional 
-      if language_data[language][:style].any?
-        language_data[language][:style] << style
-      else
-        language_data[language][:style] = style
-      end 
+      language_data.each do |language, data|
+        if language_data[language][:style].any?
+          language_data[language][:style] << style
+        else
+          language_data[language][:style] = style
+        end 
+      end
     end 
     new_hash_holder << language_data
     new_hash_holder.each do |element|
